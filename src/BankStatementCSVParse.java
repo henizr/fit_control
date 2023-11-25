b5c5e22b4c70d2e690f414bb6834b5d42ed97392
Harry <henry.iz@yandex.ru>
2023-11-25 20:08:18 +0300

    init commit

---
+++ src/BankStatementCSVParse.java
@@ -0,0 +1,6 @@
+import java.time.format.DateTimeFormatter;
+
+public class BankStatementCSVParse {
+    public static final DateTimeFormatter DATE_PATTERN =
+            DateTimeFormatter.ofPattern("dd-MM-yyyy");
+}
